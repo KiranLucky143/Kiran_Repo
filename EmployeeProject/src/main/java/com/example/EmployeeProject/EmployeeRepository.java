a1ebabdc204878bb1432ed21edc93e39d30639ba
Kiran Lucky <gellakirankumar804.com>
2025-02-17 20:18:57 +0530

    Moving Employee Project to BA73

---
+++ EmployeeProject/src/main/java/com/example/EmployeeProject/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.example.EmployeeProject;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+
+	public List<Employee> findBySalaryLessThan(Integer salary);
+	
+	
+
+}
---
+++ EmployeeProject/src/main/java/com/example/EmployeeProject/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.example.EmployeeProject;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+public interface EmployeeService {
+
+	
+	public Employee save(Employee emp);
+	
+	public List<Employee> getAllEmployees();
+
+	
+
+	public String deleteAll();
+	
+	
+	public Employee findByID(Integer id);
+	
+	public String deleteByID(Integer id);
+	
+	public List<Employee> findBySalaryLessThan(Integer sal);
+}
